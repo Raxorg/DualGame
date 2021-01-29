03b0e0a10a2fef1cb8087594da063bf055a6cdeb
Luis Frontanilla <lrdolucho@gmail.com>
2021-01-29 17:04:08 -0400

    Foundations

---
+++ desktop/src/com/frontanilla/dual/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frontanilla.dual.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frontanilla.dual.DualGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DualGame(), config);
+	}
+}
